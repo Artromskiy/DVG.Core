c5bb76e92c46c281d1b4efa7b33627485544e1e3
Artromskiy <artromskiy@gmail.com>
2025-05-20 15:00:25 +0300

    Changed folders, added CommandAttribute

---
+++ Commands/Attributes/CommandAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace DVG.Core.Commands.Attributes
+{
+    [AttributeUsage(AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+    public class CommandAttribute : Attribute { }
+}
---
+++ ICommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DVG.Core
+{
+    public interface ICommand: IComparable<ICommand>
+    {
+        public int CommandId { get; } // what
+        public int CallerId { get; } // who
+        public TimeSpan TimeStamp { get; } // when
+    }
+}
