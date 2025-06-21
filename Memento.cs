a8e3d897e4843a878e850635082bc00e70873bd1
Artromskiy <artromskiy@gmail.com>
2025-06-22 00:30:18 +0300

    Memento.ctor

--- Memento.cs
+++ Memento.cs
@@ -6,5 +6,12 @@
         public int Tick { get; }
         public int EntityId { get; }
         public T Data { get; }
+
+        public Memento(int tick, int entityId, T data)
+        {
+            Tick = tick;
+            EntityId = entityId;
+            Data = data;
+        }
     }
 }
