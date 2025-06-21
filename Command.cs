7cbdc3745047a561f303a7712c7d3116853c1fcb
Artromskiy <artromskiy@gmail.com>
2025-06-21 19:20:56 +0300

    Command and Factory

--- Command.cs
+++ Command.cs
@@ -38,6 +38,6 @@ namespace DVG.Core
         readonly int IComparable<Command<D>>.CompareTo(Command<D> other) => Tick.CompareTo(other);
         readonly int IComparable<ICommand>.CompareTo(ICommand other) => Tick.CompareTo(other.Tick);
         public readonly Command<D> WithEntityId(int entityId) => new Command<D>(entityId, ClientId, Tick, Data);
-        public readonly Command<D> WithClientId(int callerId) => new Command<D>(EntityId, callerId, Tick, Data);
+        public readonly Command<D> WithClientId(int clientId) => new Command<D>(EntityId, clientId, Tick, Data);
     }
 }
--- MVP/IFactory.cs
+++ MVP/IFactory.cs
@@ -5,12 +5,10 @@ namespace DVG.Core
     public interface IFactory<T> : IFactory
     {
         public T Create();
-        public void Dispose(T instance);
     }
     public interface IFactory<T, P> : IFactory
     {
         public T Create(P parameters);
-        public void Dispose(T instance);
     }
 
 }
