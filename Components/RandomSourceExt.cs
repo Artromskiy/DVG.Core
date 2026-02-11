680a3dd2e3991df3739a8e319964e3bea4f60d02
Artromskiy <artromskiy@gmail.com>
2026-02-12 00:51:34 +0300

    RandomSourceExt for NextRange

---
+++ Components/RandomSourceExt.cs
@@ -0,0 +1,26 @@
+﻿namespace DVG.Components
+{
+    public static class RandomSourceExt
+    {
+        private static int Next(this ref RandomSource source)
+        {
+            source.Value ^= source.Value << 13;
+            source.Value ^= source.Value >> 17;
+            source.Value ^= source.Value << 5;
+            return source.Value;
+        }
+
+        public static int NextRange(this ref RandomSource source, int min, int max)
+        {
+            var value = Next(ref source);
+            int mask = value >> 31;
+            value = (mask ^ value) - mask;
+            return value % (max - min) + min;
+        }
+
+        public static fix NextRange(this ref RandomSource source, fix min, fix max)
+        {
+            return new fix(NextRange(ref source, min.raw, max.raw + 1));
+        }
+    }
+}
