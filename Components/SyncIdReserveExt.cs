6f1314a9176a8bbdd66a3aa522011dddceda7a18
Artromskiy <artromskiy@gmail.com>
2026-02-14 04:48:22 +0300

    SyncIdReserveExt => RemainingCount, GetNext

---
+++ Components/SyncIdReserveExt.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace DVG.Components
+{
+    public static class SyncIdReserveExt
+    {
+        public static int RemainingCount(this SyncIdReserve syncIdReserve)
+        {
+            return syncIdReserve.First + syncIdReserve.Count - syncIdReserve.Current;
+        }
+
+        public static SyncId GetNext(this ref SyncIdReserve syncIdReserve)
+        {
+            Debug.Assert(syncIdReserve.Current < syncIdReserve.First + syncIdReserve.Count);
+            return syncIdReserve.Current++;
+        }
+    }
+}
