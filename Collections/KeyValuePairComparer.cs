bca59771f209d465e0b02a907a78ee7767bf6637
Artromskiy <artromskiy@gmail.com>
2026-02-15 03:27:35 +0300

    KeyValuePairComparer with IEquatable constraint

---
+++ Collections/KeyValuePairComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace System.Collections
+{
+    public sealed class KeyValuePairComparer<K, V> : EqualityComparer<KeyValuePair<K, V>>
+        where K : IEquatable<K>
+        where V : IEquatable<V>
+    {
+        public static new EqualityComparer<KeyValuePair<K, V>> Default { get; } = new KeyValuePairComparer<K, V>();
+
+        public override bool Equals(KeyValuePair<K, V> x, KeyValuePair<K, V> y) =>
+            x.Key.Equals(y.Key) && x.Value.Equals(y.Value);
+
+        public override int GetHashCode(KeyValuePair<K, V> obj) =>
+            HashCode.Combine(obj.Key, obj.Value);
+    }
+}
