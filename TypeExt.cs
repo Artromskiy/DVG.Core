bb969126d1d483bb0384f702825be2a2080328ef
Artromskiy <artromskiy@gmail.com>
2026-02-19 23:50:31 +0300

    TypeExt to get formatted generic names

---
+++ TypeExt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace DVG
+{
+    public static class TypeExt
+    {
+        public static string GetFormattedName(this Type type)
+        {
+            if (type.IsGenericType)
+            {
+                string genericArguments = type.GetGenericArguments()
+                                    .Select(x => x.Name)
+                                    .Aggregate((x1, x2) => $"{x1}, {x2}");
+                return $"{type.Name[..type.Name.IndexOf("`")]}"
+                     + $"<{genericArguments}>";
+            }
+            return type.Name;
+        }
+    }
+}
