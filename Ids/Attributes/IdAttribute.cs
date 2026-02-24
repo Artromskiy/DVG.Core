963935762c26e99e94da2ccad8b71c531618659c
Artromskiy <artromskiy@gmail.com>
2026-02-24 05:01:50 +0300

    Id with internal int

--- Ids/Attributes/IdAttribute.cs
+++ Ids/Attributes/IdAttribute.cs
@@ -1,7 +1,15 @@
 using System;
 
-namespace DVG.Core.Ids.Attributes
+namespace DVG.Ids.Attributes
 {
-    [AttributeUsage(AttributeTargets.Struct, AllowMultiple = false)]
-    public class IdAttribute : Attribute { }
+    [AttributeUsage(AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+    public class IdAttribute : Attribute
+    {
+        public readonly bool UseInt;
+
+        public IdAttribute(bool useInt)
+        {
+            UseInt = useInt;
+        }
+    }
 }
